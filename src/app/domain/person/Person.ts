e2c3e7fcc8944f20147074c5f83e458cdd6cf7d0
SerhiiSamara <ndaair@gmail.com>
2023-09-23 16:10:09 +0300

    1

---
+++ src/app/domain/person/Person.ts
@@ -0,0 +1,54 @@
+import { JsonController, Get, Post, Body, Param } from "routing-controllers";
+import { validate } from 'class-validator';
+
+import { IPerson } from "./Person.types";
+
+import { ApiResponse } from "helpers/ApiResponse";
+import { ApiError } from "helpers/ApiError";
+import { CreatePerson } from "./CreatePerson.dto";
+
+const storeData: IPerson[] = [];
+
+@JsonController("/person")
+export default class Person {
+	@Get()
+	async getAll() {
+		return new ApiResponse(true, storeData);
+	}
+
+	@Get("/:id")
+	async getOne(@Param("id") id: number): Promise<ApiResponse<IPerson | {}>> {
+		const person = storeData.find((item) => {
+			return item.id === id;
+		});
+
+		if (!person) {
+			throw new ApiError(404, {
+				code: "PERSON_NOT_FOUND",
+				message: `Person with id ${id} not found`,
+			});
+		}
+
+		return new ApiResponse(true, person);
+	}
+
+	@Post()
+	async setPerson(@Body() body: CreatePerson) {
+		// validate the body using class-validator
+		const errors = await validate(body);
+
+		if (errors.length > 0) {
+			throw new ApiError(400, {
+				message: "Validation failed",
+				code: "PERSON_VALIDATION_ERROR",
+				errors,
+			});
+		}
+
+		const id = storeData.length;
+
+		storeData.push({ ...body, id });
+
+		return new ApiResponse(true, "Person successfully created");
+	}
+}
